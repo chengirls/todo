299b90f0fea9da6fd60ced35681c1d3f71616f2d
chengirls <2233979918@qq.com>
2019-06-18 09:48:17 +0800

    链接改为别名方式

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('home/',views.home),
-    path('edit/',views.edit),
-    path('about/',views.about),
+    path('home/',views.home,name='主页'),
+    path('edit/',views.edit,name='编辑'),
+    path('about/',views.about,name='关于'),
 ]
